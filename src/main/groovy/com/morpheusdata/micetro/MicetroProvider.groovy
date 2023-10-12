5953bbf1ce926a3747888e8ad3934cd21eb0a75e
cbunge <cbunge@morpheusdata.com>
2023-10-12 18:06:36 -0400

    bumping version support for 6.3.0

--- src/main/groovy/com/morpheusdata/micetro/MicetroProvider.groovy
+++ src/main/groovy/com/morpheusdata/micetro/MicetroProvider.groovy
@@ -28,11 +28,11 @@ import groovy.text.SimpleTemplateEngine
 import groovy.json.JsonOutput
 import groovy.util.logging.Slf4j
 import org.apache.commons.net.util.SubnetUtils
-import io.reactivex.Completable
-import io.reactivex.Single
-import io.reactivex.schedulers.Schedulers
+import io.reactivex.rxjava3.core.Completable
+import io.reactivex.rxjava3.core.Single
+import io.reactivex.rxjava3.core.Scheduler
 import org.apache.http.entity.ContentType
-import io.reactivex.Observable
+import io.reactivex.rxjava3.core.Observable
 import org.apache.commons.validator.routines.InetAddressValidator
 
 @Slf4j
@@ -483,7 +483,7 @@ class MicetroProvider implements IPAMProvider, DNSProvider {
 	}
 
     def cacheZoneRecords(HttpApiClient client, NetworkPoolServer poolServer, Map opts) {
-		morpheus.network.domain.listIdentityProjections(poolServer.integration.id).flatMap {NetworkDomainIdentityProjection domain ->
+		morpheus.network.domain.listIdentityProjections(poolServer.integration.id).concatMap {NetworkDomainIdentityProjection domain ->
 			Completable.mergeArray(cacheZoneDomainRecords(client,poolServer,domain,'A',opts),
 				cacheZoneDomainRecords(client,poolServer, domain, 'AAAA', opts),
 				cacheZoneDomainRecords(client,poolServer, domain, 'PTR', opts),
@@ -493,7 +493,7 @@ class MicetroProvider implements IPAMProvider, DNSProvider {
 			).toObservable().subscribeOn(Schedulers.io())
 		}.doOnError{ e ->
 			log.error("cacheZoneRecords error: ${e}", e)
-		}.subscribe()
+		}.blockingSubscribe()
 	}  
 
 	Completable cacheZoneDomainRecords(HttpApiClient client, NetworkPoolServer poolServer, NetworkDomainIdentityProjection domain, String recordType, Map opts) {
@@ -970,9 +970,9 @@ class MicetroProvider implements IPAMProvider, DNSProvider {
 	// cacheIpAddressRecords
     void cacheIpAddressRecords(HttpApiClient client, NetworkPoolServer poolServer, Map opts=[:]) {
         
-        morpheus.network.pool.listIdentityProjections(poolServer.id).buffer(50).flatMap { Collection<NetworkPoolIdentityProjection> poolIdents ->
+        morpheus.network.pool.listIdentityProjections(poolServer.id).buffer(50).concatMap { Collection<NetworkPoolIdentityProjection> poolIdents ->
             return morpheus.network.pool.listById(poolIdents.collect{it.id})
-        }.flatMap { NetworkPool pool ->
+        }.concatMap { NetworkPool pool ->
             def listResults = listHostRecords(client,poolServer,pool)
             if (listResults.success && !listResults.error && listResults.data) {
                 def customProperty = poolServer.configMap?.nameProperty.toString()
@@ -1003,7 +1003,7 @@ class MicetroProvider implements IPAMProvider, DNSProvider {
             }
         }.doOnError{ e ->
             log.error("cacheIpRecords error: ${e}", e)
-        }.subscribe()
+        }.blockingSubscribe()
 
     }
 
